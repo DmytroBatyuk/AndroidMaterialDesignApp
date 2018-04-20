cbd92c934e794c2ddfa70687f87e5f7185e4a0ea
DmytroBatyuk <dimabatyuk@gmail.com>
2018-04-20 14:52:19 +0300

    added placeholder which takes each view(button) which is clicked

--- app/src/main/java/ua/batyuk/dmytro/androidmaterialdesignapp/MainActivity.kt
+++ app/src/main/java/ua/batyuk/dmytro/androidmaterialdesignapp/MainActivity.kt
@@ -1,12 +1,34 @@
 package ua.batyuk.dmytro.androidmaterialdesignapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.constraint.Placeholder
+import android.support.v7.app.AppCompatActivity
+import android.transition.TransitionManager
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var placeholder: Placeholder
+    private lateinit var constraintLayout: ConstraintLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        constraintLayout = findViewById(R.id.root)
+        placeholder = findViewById(R.id.placeholder)
+        setOnClickListener(findViewById<View>(R.id.button_1))
+        setOnClickListener(findViewById<View>(R.id.button_2))
+        setOnClickListener(findViewById<View>(R.id.button_3))
+        setOnClickListener(findViewById<View>(R.id.button_4))
+    }
+
+    private fun setOnClickListener(view: View) {
+        view.setOnClickListener {
+            //To make changes on placeholder view visible(snoozed)
+            TransitionManager.beginDelayedTransition(constraintLayout)
+
+            placeholder.setContentId(it.id)
+        }
     }
 }
