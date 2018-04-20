5aa7fe988a755205ba97046efd945c5e83cacb7a
DmytroBatyuk <dimabatyuk@gmail.com>
2018-04-20 12:14:33 +0300

    added enabling\disabling group of views with button (need to use constrain layout 1.1.0 version)

--- app/src/main/java/ua/batyuk/dmytro/androidmaterialdesignapp/MainActivity.kt
+++ app/src/main/java/ua/batyuk/dmytro/androidmaterialdesignapp/MainActivity.kt
@@ -2,11 +2,23 @@ package ua.batyuk.dmytro.androidmaterialdesignapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.Group
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.button_1)?.setOnClickListener {
+            val group = findViewById<Group>(R.id.group_1)
+            group.visibility = if (group.visibility == View.VISIBLE) View.GONE else View.VISIBLE
+        }
+
+        findViewById<View>(R.id.button_2)?.setOnClickListener {
+            val group = findViewById<Group>(R.id.group_2)
+            group.visibility = if (group.visibility == View.VISIBLE) View.GONE else View.VISIBLE
+        }
     }
 }
