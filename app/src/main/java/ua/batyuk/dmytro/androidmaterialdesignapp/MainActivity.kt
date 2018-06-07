572878c2112b15430d806688e06fb49ed1c4ff2b
DmytroBatyuk <dimabatyuk@gmail.com>
2018-06-07 11:41:46 +0300

    added collapsing behavior for toolbar in coordinator layout. Added circle progress bar just to show something on toolbar

--- app/src/main/java/ua/batyuk/dmytro/androidmaterialdesignapp/MainActivity.kt
+++ app/src/main/java/ua/batyuk/dmytro/androidmaterialdesignapp/MainActivity.kt
@@ -1,12 +1,50 @@
 package ua.batyuk.dmytro.androidmaterialdesignapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+//        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+//        setSupportActionBar(toolbar)
+
+        var recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = Adapter()
     }
+}
+
+private class Adapter : RecyclerView.Adapter<ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return 50
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+}
+
+private class ViewHolder(v: View): RecyclerView.ViewHolder(v) {
+    private val textView = v.findViewById<TextView>(R.id.textView)
+    fun bind(position: Int) {
+        textView.text = position.toString()
+    }
+
 }
